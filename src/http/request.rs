63c613eae411db047baa13ee402a1cad2bd207bd
DrainGangScholar <veljko.markovic@elfak.rs>
2023-04-27 15:11:43 +0200

    Pocinje da lici na nesto...

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,6 +1,13 @@
 use super::method::Method;
+use std::convert::TryFrom;
 pub struct Request {
     path: String,
     query: Option<String>,
     method: Method,
+}
+impl TryFrom<&[u8]> for Request {
+    type Error = String;
+    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+        unimplemented!()
+    }
 }
--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,8 @@
+use crate::http::request;
+use crate::http::Request;
+use std::convert::TryFrom;
+use std::convert::TryInto;
+use std::io::Read;
 use std::net::TcpListener;
 pub struct Server {
     address: String,
@@ -14,9 +19,20 @@ impl Server {
 
         loop {
             match listener.accept() {
-                Ok((stream, _)) => {
-                    let a = 5;
-                    println!("OK");
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            print!("Received a request: {}", String::from_utf8_lossy(&buffer));
+                            match Request::try_from(&buffer[..]) {
+                                Ok(request) => {}
+                                Err(e) => println!("Failed to parse a request; {}", e),
+                            }
+                        }
+                        Err(e) => {
+                            println!("Failed to read from connection: {}", e);
+                        }
+                    }
                 }
                 Err(e) => println!("Failed to establish a connection: {}", e),
             }
