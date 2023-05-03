1aab869a530d165300917de4a9498ab3dc29e143
DrainGangScholar <veljko.markovic@elfak.rs>
2023-05-03 15:05:25 +0200

    Lifetimes... Errors.... Svasta...

--- src/server.rs
+++ src/server.rs
@@ -20,11 +20,11 @@ impl Server {
         loop {
             match listener.accept() {
                 Ok((mut stream, _)) => {
-                    let mut buffer = [0; 1024];
-                    match stream.read(&mut buffer) {
+                    let mut buf = [0; 1024];
+                    match stream.read(&mut buf) {
                         Ok(_) => {
-                            print!("Received a request: {}", String::from_utf8_lossy(&buffer));
-                            match Request::try_from(&buffer[..]) {
+                            print!("Received a request: {}", String::from_utf8_lossy(&buf));
+                            match Request::try_from(&buf[..]) {
                                 Ok(request) => {}
                                 Err(e) => println!("Failed to parse a request; {}", e),
                             }
